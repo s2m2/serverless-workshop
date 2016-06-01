8e1a5d897a1b57164abe00f0242eeb5cb3f1bdef
Hiroki Murashige <hiroki-m@gaia.eonet.ne.jp>
2016-06-02 00:09:34 +0900

    s3から呼び出すlambda関数を追加(次の関数呼び出しは未実装)

--- endpoint/uploader.py
+++ endpoint/uploader.py
@@ -3,12 +3,13 @@
 
 import sys
 import os
-import boto3 
+import boto3
+import datetime
 
 __author__ = 'hiroki8080'
 
 bucket_name = 'siimii-compression-upload'
-key_name = 'file_upload'
+key_name = 'upload_' + datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
 
 argvs = sys.argv
 argc = len(argvs)
@@ -19,10 +20,10 @@ if(argc != 2):
     print 'Only one can be specified as an argument.'
     quit()
 
-if 'AWS_ACCESS_KEY_ID' not in os.environ  or 'AWS_SECRET_ACCESS_KEY' not in os.environ:
-        print 'Set the environment variable of AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.'
-        quit()
+if 'AWS_ACCESS_KEY_ID' not in os.environ or 'AWS_SECRET_ACCESS_KEY' not in os.environ:
+    print 'Set the environment variable of AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.'
+    quit()
 
-s3 = boto3.resource('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+s3 = boto3.resource('s3', aws_access_key_id=os.environ[
+                    'AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 s3.Bucket(bucket_name).upload_file(argvs[1], key_name)
-
